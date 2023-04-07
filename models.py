c87dfc97e2a5c891ee1bd361b2d722930f7a7cab
404Simon <simsicompi@mail.de>
2023-04-07 14:35:51 +0200

    WIP: parsing more fields of appointment

--- models.py
+++ models.py
@@ -9,3 +9,5 @@ class Appointment:
     subject: str
     room: str
     teacher: str
+    def __repr__(self):
+        return f'{self.date.strftime("%d.%m.%Y")} ({self.start_time.strftime("%H:%M")} - {self.end_time.strftime("%H:%M")}) - {self.subject} - {self.room} - {self.teacher}'
